f890b6b8d64d9c89eb43d87210e4419064318a04
jszymanski <jacek.p.szymanski@gmail.com>
2023-05-26 00:00:00 +0200

    feat: embedding civetweb, attempt cross compile (failed)

--- default.nix
+++ default.nix
@@ -1,13 +1,32 @@
-{ pkgs ? import <nixpkgs> {} }:
+{ stdenv, cmake, civetweb }:
 
-with pkgs;
+let
+  civetweb_embed = civetweb.overrideAttrs (oldAttrs: {
+    pname = "civetweb_embed";
+    cmakeFlags = [
+      "-DBUILD_SHARED_LIBS=OFF"
+      "-DCIVETWEB_ENABLE_CXX=OFF"
+      "-DCIVETWEB_ENABLE_IPV6=OFF"
+      "-DCIVETWEB_ENABLE_SERVER_EXECUTABLE=OFF"
+      "-DCIVETWEB_ENABLE_SSL=OFF"
+      # The civetweb unit tests rely on downloading their fork of libcheck.
+      "-DCIVETWEB_BUILD_TESTING=OFF"
+    ];
+    patches = [ ./cwpatch/mingw-cross.patch ];
+  });
+in
 stdenv.mkDerivation rec {
-  name = "realworld-c-civetweb";
+  pname = "realworld-c-civetweb";
   version = "0.1.0";
 
   src = ./src;
 
   nativeBuildInputs = [ cmake ];
-  buildInputs = [ civetweb ];
+  buildInputs = [ civetweb_embed ];
+
+  cmakeFlags = [
+    # FIXME: This is a hack to get the civetweb_embed library to be found.
+   "-Dcivetweb_LIBRARY_DIR=${civetweb_embed}/lib"
+  ];
 
 }
