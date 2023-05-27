8a42ea74d58669dc83d41cbd6ec0faa35e39fe81
jszymanski <jacek.p.szymanski@gmail.com>
2023-05-28 00:00:00 +0200

    chore: add dependency on sqlite

--- default.nix
+++ default.nix
@@ -1,6 +1,7 @@
 { stdenv, cmake, nur }:
 
 let civetweb-dev = nur.repos.jacekszymanski.civetweb-dev;
+    sqlite-dev = nur.repos.jacekszymanski.sqlite-dev;
 in
 stdenv.mkDerivation rec {
   pname = "realworld-c-civetweb";
@@ -9,10 +10,10 @@ stdenv.mkDerivation rec {
   src = ./src;
 
   nativeBuildInputs = [ cmake ];
-  buildInputs = [ civetweb-dev ];
+  buildInputs = [ civetweb-dev sqlite-dev ];
 
   preConfigure = ''
-    export LDFLAGS="-L${civetweb-dev}/lib -lcivetweb"
+    export LDFLAGS="-L${civetweb-dev}/lib -lcivetweb -L${sqlite-dev}/lib -lsqlite3"
     echo set LDFLAGS to $LDFLAGS
   '';
 
