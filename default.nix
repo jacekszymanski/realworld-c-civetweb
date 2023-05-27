4962fbd872f616563565a1cfa65e9a5669832096
jszymanski <jacek.p.szymanski@gmail.com>
2023-05-28 00:00:00 +0200

    chore: use civetweb from (my unpublished) nur

--- default.nix
+++ default.nix
@@ -1,19 +1,6 @@
-{ stdenv, cmake, civetweb }:
+{ stdenv, cmake, nur }:
 
-let
-  civetweb_embed = civetweb.overrideAttrs (oldAttrs: {
-    pname = "civetweb_embed";
-    cmakeFlags = [
-      "-DBUILD_SHARED_LIBS=OFF"
-      "-DCIVETWEB_ENABLE_CXX=OFF"
-      "-DCIVETWEB_ENABLE_IPV6=OFF"
-      "-DCIVETWEB_ENABLE_SERVER_EXECUTABLE=OFF"
-      "-DCIVETWEB_ENABLE_SSL=OFF"
-      # The civetweb unit tests rely on downloading their fork of libcheck.
-      "-DCIVETWEB_BUILD_TESTING=OFF"
-    ];
-    patches = [ ./cwpatch/mingw-cross.patch ];
-  });
+let civetweb-dev = nur.repos.jacekszymanski.civetweb-dev;
 in
 stdenv.mkDerivation rec {
   pname = "realworld-c-civetweb";
@@ -22,11 +9,11 @@ stdenv.mkDerivation rec {
   src = ./src;
 
   nativeBuildInputs = [ cmake ];
-  buildInputs = [ civetweb_embed ];
+  buildInputs = [ civetweb-dev ];
 
   cmakeFlags = [
     # FIXME: This is a hack to get the civetweb_embed library to be found.
-   "-Dcivetweb_LIBRARY_DIR=${civetweb_embed}/lib"
+   "-Dcivetweb_LIBRARY_DIR=${civetweb-dev}/lib"
   ];
 
 }
