22f4c302e6c9d622e5187024d6c9b7c2db84b955
jszymanski <jacek.p.szymanski@gmail.com>
2023-05-29 00:00:00 +0200

    chore: add l8w8jwt (what a name!) to deps

--- default.nix
+++ default.nix
@@ -1,8 +1,5 @@
 { stdenv, cmake, nur }:
 
-let civetweb-dev = nur.repos.jacekszymanski.civetweb-dev;
-    sqlite-dev = nur.repos.jacekszymanski.sqlite-dev;
-in
 stdenv.mkDerivation rec {
   pname = "realworld-c-civetweb";
   version = "0.1.0";
@@ -10,11 +7,7 @@ stdenv.mkDerivation rec {
   src = ./src;
 
   nativeBuildInputs = [ cmake ];
-  buildInputs = [ civetweb-dev sqlite-dev ];
-
-  preConfigure = ''
-    export LDFLAGS="-L${civetweb-dev}/lib -lcivetweb -L${sqlite-dev}/lib -lsqlite3"
-    echo set LDFLAGS to $LDFLAGS
-  '';
+  buildInputs = with nur.repos.jacekszymanski;
+    [ civetweb-dev sqlite-dev l8w8jwt-dev ];
 
 }
